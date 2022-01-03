5e34c060d2d43623450e151b1381baf02d8b4e66
Ramis <dantyu4@mail.ru>
2022-01-03 20:50:52 +0300

    New header

--- index.py
+++ index.py
@@ -137,4 +137,3 @@ def make_report(log_file_name, report_template_file_name, report_output_file_nam
     wb.save(dest_filename)
     pass
 
-make_report('logs.xlsx','report_template.xlsx','report.xlsx')
